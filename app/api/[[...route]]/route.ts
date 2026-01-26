56d15ac65a2cd3742665cfade0293b53abec00d8
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2026-01-27 00:46:47 +0530

    communities

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,41 +1,83 @@
 import { db } from '@/db'
-import { communities, messages } from '@/db/schema'
+import { communities, communityMembers } from '@/db/schema'
 import { auth } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
 import { HTTPException } from 'hono/http-exception'
+import { handle } from 'hono/vercel'
 
 type Variables = {
-  usdrId: string
+  userId: string | null
 }
 
 const app = new Hono<{ Variables: Variables }>().basePath('/api')
 
 app.onError((err, c) => {
-  console.error(err)
-  return c.json({ err: 'Internal Server Error' }, 500)
+  console.error('API Error:', err)
+
+  if (err instanceof HTTPException) {
+    return err.getResponse()
+  }
+
+  if (
+    err instanceof Error &&
+    (err.message.includes('violates') ||
+      err.message.includes('constraint') ||
+      err.message.includes('duplicate key'))
+  ) {
+    return c.json({ error: 'Invalid data' }, 400)
+  }
+  if (err instanceof Error && err.message.includes('not found')) {
+    return c.json({ error: 'Not found' }, 404)
+  }
+
+  return c.json({ error: 'Internal Server Error' }, 500)
 })
 
-app.get('/*', async (c, next) => {
-  const publicRoute = ['/api/communities/all']
-  if (publicRoute.includes(c.req.path)) {
+app.use(async (c, next) => {
+  const publicPaths = ['/com/all']
+  if (publicPaths.includes(c.req.path)) {
     return next()
   }
 
-  const session = await auth()
-  if (!session.userId) {
+  const { userId } = await auth()
+  if (!userId) {
     throw new HTTPException(401, { message: 'Unauthorized' })
   }
 
-  c.set('usdrId', session.userId)
-
-  return await next()
+  c.set('userId', userId)
+  return next()
 })
 
-app.get('/communities/all', async c => {
-  const allCommu = await db.select().from(communities)
-  return c.json(allCommu)
+app.get('/com/all', async c => {
+  const com = await db.select().from(communities)
+  return c.json(com)
 })
 
+app.post('/com/:comId/join', async c => {
+  const userId = c.get('userId')
+  if (!userId) throw new HTTPException(401, { message: 'Unauthorized' })
+
+  const comId = c.req.param('comId')
+  const [com] = await db
+    .select()
+    .from(communities)
+    .where(eq(communities.id, comId))
+
+  if (!com) {
+    throw new HTTPException(404, { message: 'Community Not Found' })
+  }
+
+  await db.insert(communityMembers).values({
+    userId,
+    communityId: comId
+  })
+
+  return c.json({ message: 'Joined community' })
+})
+
+export const dynamic = 'force-dynamic'
 export const GET = handle(app)
 export const POST = handle(app)
+export const PUT = handle(app)
+export const DELETE = handle(app)
